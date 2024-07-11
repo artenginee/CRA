a95fdf29d3cb89fd991bc1b83a3f283f7d35b0fc
tae-eui.kim <taeeui.k@gmail.com>
2024-07-11 16:56:46 +0900

    Mock 실습 예제 : DeviceDriver 중간코드

---
+++ TDD/TestDouble/DeviceDriver/device_driver.py
@@ -0,0 +1,46 @@
+import time
+
+from hardware_interface import FlashMemoryDevice
+
+
+class DeviceDriver:
+    """
+    This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
+    """
+
+    def __init__(self, device: FlashMemoryDevice):
+        """
+        :type device: hardware_interface.FlashMemoryDevice
+        """
+        self.__device = device
+
+    def write(self, address: int, data: int) -> None:
+        # TODO: implement this method
+        if self.__device.read(address) != 0xFF:
+            raise Exception('WriteFailException')
+
+        self.__device.write(address, data)
+
+    def read(self, address: int) -> int:
+        # TODO: implement this method
+        result = 0x0
+        lst = []
+        for i in range(5):
+            result = self.__device.read(address)
+            if i == 0:
+                lst.append(result)
+            else:
+                if lst[i - 1] == result:
+                    lst.append(result)
+                else:
+                    raise Exception('ReadFailException')
+
+            time.sleep(0.2)
+
+        return result
+
+    def read_and_print(self, start_addr: int, end_addr: int) -> str:
+        pass
+
+    def write_all(self, value: int) -> None:
+        pass
---
+++ TDD/TestDouble/DeviceDriver/test_device_driver.py
@@ -0,0 +1,42 @@
+from unittest import TestCase, skip
+from unittest.mock import Mock, patch
+from hardware_interface import FlashMemoryDevice
+from device_driver import DeviceDriver
+
+
+class DeviceDriverTest(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.hardware: FlashMemoryDevice = Mock()
+        self.driver = DeviceDriver(self.hardware)
+
+    # @patch.object(FlashMemoryDevice, 'read', side_effect=[0x10, 0x10, 0x10, 0x10, 0x10])
+    def test_read_success(self):
+        self.hardware.read.side_effect = [0x10, 0x10, 0x10, 0x10, 0x10]
+        self.assertEqual(0x10, self.driver.read(0xFF))
+        self.assertEqual(5, self.hardware.read.call_count)
+
+    def test_read_fail(self):
+        self.hardware.read.side_effect = [0x10, 0x10, 0x10, 0x11, 0x10]
+
+        with self.assertRaises(Exception) as context:
+            print(self.driver.read(0xFF))
+
+        self.assertEqual('ReadFailException', str(context.exception))
+
+    def test_write_success(self):
+        self.hardware.read.return_value = 0xFF
+        self.assertIsNone(self.driver.write(0xFF, 0x10))
+        self.assertEqual(1, self.hardware.read.call_count)
+
+    def test_write_fail(self):
+        self.hardware.read.return_value = 0x10
+
+        with self.assertRaises(Exception) as context:
+            self.driver.write(0xFF, 0x10)
+
+        self.assertEqual('WriteFailException', str(context.exception))
+
+    def test_read_and_print(self):
+        pass
